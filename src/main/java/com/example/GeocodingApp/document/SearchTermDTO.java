51f22a16d14b17bd6a4dfd498ff214eecb9f462b
Safaa El Khayari <safaa.dk.1999@gmail.com>
2022-04-14 21:23:47 +0000

    Just commiting

---
+++ src/main/java/com/example/GeocodingApp/document/SearchTermDTO.java
@@ -0,0 +1,26 @@
+package com.example.GeocodingApp.document;
+
+import java.util.List;
+
+public class SearchTermDTO {
+    private List<String> fields;
+    private String searchTerm;
+
+
+    public List<String> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<String> fields) {
+        this.fields = fields;
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+
+}
