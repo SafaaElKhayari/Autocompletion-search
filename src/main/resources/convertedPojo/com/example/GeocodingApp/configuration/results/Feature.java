8f37ddeeea3cac0cdd24fb046d1414c3bb86fd5a
Safaa El Khayari <safaa.dk.1999@gmail.com>
2022-04-21 10:59:16 +0000

    Just commiting

---
+++ src/main/resources/convertedPojo/com/example/GeocodingApp/configuration/results/Feature.java
@@ -0,0 +1,142 @@
+
+package com.example.GeocodingApp.configuration.results;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "type",
+    "properties",
+    "geometry"
+})
+@Generated("jsonschema2pojo")
+public class Feature {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("properties")
+    private Properties properties;
+    @JsonProperty("geometry")
+    private Geometry geometry;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Feature withType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    @JsonProperty("properties")
+    public Properties getProperties() {
+        return properties;
+    }
+
+    @JsonProperty("properties")
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public Feature withProperties(Properties properties) {
+        this.properties = properties;
+        return this;
+    }
+
+    @JsonProperty("geometry")
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    @JsonProperty("geometry")
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public Feature withGeometry(Geometry geometry) {
+        this.geometry = geometry;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public Feature withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Feature.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("type");
+        sb.append('=');
+        sb.append(((this.type == null)?"<null>":this.type));
+        sb.append(',');
+        sb.append("properties");
+        sb.append('=');
+        sb.append(((this.properties == null)?"<null>":this.properties));
+        sb.append(',');
+        sb.append("geometry");
+        sb.append('=');
+        sb.append(((this.geometry == null)?"<null>":this.geometry));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.geometry == null)? 0 :this.geometry.hashCode()));
+        result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
+        result = ((result* 31)+((this.type == null)? 0 :this.type.hashCode()));
+        result = ((result* 31)+((this.properties == null)? 0 :this.properties.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Feature) == false) {
+            return false;
+        }
+        Feature rhs = ((Feature) other);
+        return (((((this.geometry == rhs.geometry)||((this.geometry!= null)&&this.geometry.equals(rhs.geometry)))&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))))&&((this.type == rhs.type)||((this.type!= null)&&this.type.equals(rhs.type))))&&((this.properties == rhs.properties)||((this.properties!= null)&&this.properties.equals(rhs.properties))));
+    }
+
+}
