c39510e824327359d7e8ec93df09737255ae8a94
Safaa El Khayari <safaa.dk.1999@gmail.com>
2022-05-31 09:45:24 +0100

    all commit

---
+++ src/main/java/com/example/GeocodingApp/service/OSMAddressService.java
@@ -0,0 +1,19 @@
+package com.example.GeocodingApp.service;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class OSMAddressService {
+
+
+    public String  getAddress (String address){
+
+
+
+
+
+        return " ";
+
+    }
+
+}
